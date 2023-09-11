505896b241045a15073761439a9d78cd18fbf2bb
Joel <ramosjoel@microsoft.com>
2023-09-11 13:42:20 -0700

    init commit for wiki UX project

---
+++ UX/wiki-ux/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WikiComponent from './components/WikiComponent';
+
+function App() {
+  return (
+    <section className="wrapper">
+      <h1>Wiki UX application</h1>
+      <WikiComponent />
+    </section>
+  );
+}
+
+export default App;
