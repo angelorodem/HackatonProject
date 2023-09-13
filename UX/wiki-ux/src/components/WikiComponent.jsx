dd0532b0e071ca9b450966a8b3db365ae6ca9e71
Joel <ramosjoel@microsoft.com>
2023-09-13 14:52:41 -0700

    initial commit for this enhancement

--- UX/wiki-ux/src/components/WikiComponent.jsx
+++ UX/wiki-ux/src/components/WikiComponent.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-import WikiSidebarComponent from './WikiSidebarComponent'
-
 export default function WikiComponent() {
     const [QnAs, setQnAs] = useState([]);
+    const [title, setTitle] = useState("DRI Channel");
 
     useEffect(() => {
-        fetch('https://extractqnaapi2.azure-api.net/ConversationSummary',{
-                method: 'GET',
-                accept: 'text/plain'
-            }).then((response) => response.json())
+        fetch(`https://extractqnaapi2.azure-api.net/ConversationSummary?channelName=${title}`, {
+            method: 'GET',
+            accept: 'text/plain'
+        }).then((response) => response.json())
             .then((data) => {
                 console.log("data is " + JSON.stringify(data));
                 setQnAs(data);
@@ -17,23 +16,62 @@ export default function WikiComponent() {
             .catch((err) => {
                 console.log("Failed with error" + err);
             });
-    }, []);
+    }, [title]);
+
+    // async function fetchData(channelName) {
+    //     try {
+    //         fetch(`https://extractqnaapi2.azure-api.net/ConversationSummary?channelName=${channelName}`, {
+    //         method: 'GET',
+    //         accept: 'text/plain'
+    //     }).then((response) => response.json())
+    //         .then((data) => {
+    //             console.log("data is " + JSON.stringify(data));
+    //             setQnAs(data);
+    //         })
+    //         .catch((err) => {
+    //             console.log("Failed with error" + err);
+    //         });
+    //     } catch (error) {
+    //         console.error("Error fetching data: ", error);
+    //     }
+    // }
+
+    const handleButtonClick = async (channelName) => {
+        setTitle(channelName);
+        // await fetchData(channelName);
+    }
 
     return (
         <div className="wiki-section">
-            <WikiSidebarComponent />
+            <div className="wiki-sidebar">
+                <h2>Channels</h2>
+                <ul>
+                    <li>
+                        <a 
+                            href="/"
+                            onClick={() => handleButtonClick("DRI Channel")}
+                        >DRI Channel</a>
+                    </li>
+                    <li>
+                        <a 
+                            href="/"
+                            onClick={() => handleButtonClick("Engineering Channel")}
+                        >Engineering Channel</a>
+                    </li>
+                </ul>
+            </div>
             <div className="wiki-main-content">
-                <h1>Channel Wiki</h1>
+                <h1>{title} Wiki</h1>
                 <div className="wiki-content">
-                   {QnAs.map((QnA) => {
-                    return (
-                        <div className="wiki-content-item" key={QnA.conversationId}>
-                            <h2 className="wiki-item-title">{QnA.question}</h2>
-                            <p className="post-body">{QnA.summary}</p>
-                        </div>
-                    );
-                  })}
-               </div>
+                    {QnAs && QnAs.length !== 0 && QnAs.map((QnA) => {
+                        return (
+                            <div key={QnA.conversationId} className="wiki-content-item">
+                                <h2 className="wiki-item-title">{QnA.question}</h2>
+                                <p className="post-body">{QnA.summary}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     )
