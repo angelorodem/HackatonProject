cd7408da93204f4e6aeb4f763083074384ba4967
Joel <ramosjoel@microsoft.com>
2023-09-13 13:04:58 -0700

    pushing changes to WikiComponent to fetched from prod endpoint

--- UX/wiki-ux/src/components/WikiComponent.jsx
+++ UX/wiki-ux/src/components/WikiComponent.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 import WikiSidebarComponent from './WikiSidebarComponent'
 
 export default function WikiComponent() {
+    const [QnAs, setQnAs] = useState([]);
+
+    useEffect(() => {
+        fetch('https://extractqnaapi2.azure-api.net/ConversationSummary',{
+                method: 'GET',
+                accept: 'text/plain'
+            }).then((response) => response.json())
+            .then((data) => {
+                console.log("data is " + JSON.stringify(data));
+                setQnAs(data);
+            })
+            .catch((err) => {
+                console.log("Failed with error" + err);
+            });
+    }, []);
+
     return (
         <div className="wiki-section">
             <WikiSidebarComponent />
             <div className="wiki-main-content">
-                <h1>Wiki Name</h1>
+                <h1>Channel Wiki</h1>
                 <div className="wiki-content">
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                    <div className="wiki-content-item">
-                        <h2 className="wiki-item-title">Question title</h2>
-                        <p className="wiki-item-answer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae repellendus veritatis molestiae est deleniti. Distinctio officia nulla incidunt, quam odio vero dolores blanditiis temporibus quod ad! Quaerat suscipit odit laborum.</p>
-                    </div>
-                </div>
+                   {QnAs.map((QnA) => {
+                    return (
+                        <div className="wiki-content-item" key={QnA.conversationId}>
+                            <h2 className="wiki-item-title">{QnA.question}</h2>
+                            <p className="post-body">{QnA.summary}</p>
+                        </div>
+                    );
+                  })}
+               </div>
             </div>
         </div>
     )
--- UX/wiki-ux/src/components/WikiSidebarComponent.jsx
+++ UX/wiki-ux/src/components/WikiSidebarComponent.jsx
@@ -4,8 +4,6 @@ export default function WikiSidebarComponent() {
             <h2>Channels</h2>
             <ul>
                 <li><a href="/">Channel 1</a></li>
-                <li><a href="/">Channel 2</a></li>
-                <li><a href="/">Channel 3</a></li>
             </ul>
         </div>
     )
